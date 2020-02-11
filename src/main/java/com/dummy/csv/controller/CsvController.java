02c92a01384b41db611010b5512c580d6f873510
Sachin Y <waysachin2@gmail.com>
2020-02-10 19:37:11 -0500

    Updated output and supported mediatype

--- src/main/java/com/dummy/csv/controller/CsvController.java
+++ src/main/java/com/dummy/csv/controller/CsvController.java
@@ -3,8 +3,11 @@ package com.dummy.csv.controller;
 import java.io.InputStream;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.dummy.csv.service.CsvJsonService;
@@ -17,6 +20,8 @@ public class CsvController {
 
 	@POST
 	@Path("/csv")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	public Response convertToCsv(InputStream inputStream) throws Exception {
 		StringBuilder retult = csvService.convertToCsv(inputStream);
 		return Response.status(200).entity(retult.toString()).build();
@@ -24,6 +29,8 @@ public class CsvController {
 
 	@POST
 	@Path("/json")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response convertToJson(InputStream inputStream) throws Exception {
 		String retult = csvService.convertToJson(inputStream);
 		return Response.status(200).entity(retult).build();
